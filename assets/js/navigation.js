5037a57c8b4012ddd0eca2ef8eb6529f7c73c00d
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-03-25 13:29:31 +0900

    Dropdown : fixed error

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,8 +1,8 @@
 const dropDown = document.querySelector("#jsDropDown");
-const dropBtn = dropDown.querySelector("i");
-const dropDownContent = dropDown.querySelector("#jsDropDownContent");
 
 const handleHover = (event) => {
+  const dropBtn = dropDown.querySelector("i");
+  const dropDownContent = dropDown.querySelector("#jsDropDownContent");
   dropDownContent.classList.toggle("dropdown-show");
   if (event.target !== dropBtn) {
     if (dropDownContent.classList.contains("drop-down-show"))
@@ -13,7 +13,6 @@ const handleHover = (event) => {
 function init() {
   window.addEventListener("click", handleHover);
 }
-
 if (dropDown) {
   init();
 }
