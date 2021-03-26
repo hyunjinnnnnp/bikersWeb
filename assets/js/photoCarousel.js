6635479052d4788824de9699bb2c74b15ab367fe
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-03-26 19:23:10 +0900

    photo detail, home: carousel

--- assets/js/photoCarousel.js
+++ assets/js/photoCarousel.js
@@ -1,50 +1,55 @@
-const nextBtnList = document.querySelectorAll(".carousel__next-i");
-const prevBtnList = document.querySelectorAll(".carousel__prev-i");
+const photoDetailCarousel = document.querySelector("#jsPhotoDetailCarousel");
+const photoBlocks = document.querySelectorAll(".photoBlock");
 
-const carouselContainer = document.querySelector("#jsCarouselContainer");
 let targetBlock;
 let nextBtn;
 let prevBtn;
 const IMG_CLASS_NAME = "carousel__photo";
-const NEXT_CLASS = "carousel__next-i";
-const PREV_CLASS = "carousel__prev-i";
 const HIDE = "carousel__btn-hide";
 const SHOW = "carousel__btn-show";
 
+let targetBtn;
 let imgs;
 let oldActived;
 let activedElem;
 
-const togglePrevBtn = () => {
+const toggleBtn = () => {
   if (activedElem === activedElem.parentNode.firstChild) {
-    //HIDE  //첫번쨰 사진이라면
-    prevBtn.classList.remove(SHOW);
+    //첫번째 사진일 때
+    if (prevBtn.classList.contains(SHOW)) {
+      prevBtn.classList.remove(SHOW);
+    }
     prevBtn.classList.add(HIDE);
-    //SHOW NEXT BUTTON
-  } else {
-    //SHOW
-    nextBtn = prevBtn.nextSibling;
     if (nextBtn.classList.contains(HIDE)) {
       nextBtn.classList.remove(HIDE);
     }
     nextBtn.classList.add(SHOW);
-  }
-};
-const toggleNextBtn = () => {
-  if (activedElem === activedElem.parentNode.lastChild) {
-    nextBtn.classList.remove(SHOW);
-    nextBtn.classList.add(HIDE);
-  } else {
-    //SHOW
+  } else if (activedElem === activedElem.parentNode.lastChild) {
+    //마지막 인덱스 라면
     prevBtn = nextBtn.previousSibling;
     if (prevBtn.classList.contains(HIDE)) {
       prevBtn.classList.remove(HIDE);
     }
     prevBtn.classList.add(SHOW);
+    if (nextBtn.classList.contains(SHOW)) {
+      nextBtn.classList.remove(SHOW);
+    }
+    nextBtn.classList.add(HIDE);
+  } else {
+    prevBtn = nextBtn.previousSibling;
+    nextBtn = prevBtn.nextSibling;
+    if (prevBtn.classList.contains(HIDE)) {
+      prevBtn.classList.remove(HIDE);
+      prevBtn.classList.add(SHOW);
+    }
+    if (nextBtn.classList.contains(HIDE)) {
+      nextBtn.classList.remove(HIDE);
+      nextBtn.classList.add(SHOW);
+    }
   }
 };
-const moveNext = (event) => {
-  [nextBtn, targetBlock] = event.path;
+
+const moveNext = () => {
   oldActived = targetBlock.querySelector(".active");
   if (oldActived.previousSibling) {
     oldActived.previousSibling.className = IMG_CLASS_NAME;
@@ -52,40 +57,63 @@ const moveNext = (event) => {
   oldActived.className = IMG_CLASS_NAME;
   activedElem = oldActived.nextSibling;
   activedElem.className = `${IMG_CLASS_NAME} active`;
-  if (activedElem.nextSibling) {
+  if (activedElem !== activedElem.parentNode.lastChild) {
     activedElem.nextSibling.className = `${IMG_CLASS_NAME} next`;
   }
-  if (activedElem.previousSibling) {
+  if (activedElem !== activedElem.parentNode.firstChild) {
     activedElem.previousSibling.className = `${IMG_CLASS_NAME} prev`;
   }
-  toggleNextBtn();
+  toggleBtn();
 };
-const movePrev = (event) => {
-  [prevBtn, targetBlock] = event.path;
+
+const movePrev = () => {
   oldActived = targetBlock.querySelector(".active");
   oldActived.className = IMG_CLASS_NAME;
   activedElem = oldActived.previousSibling;
   activedElem.className = `${IMG_CLASS_NAME} active`;
-  if (activedElem.previousSibling) {
+  if (activedElem !== activedElem.parentNode.firstChild) {
     activedElem.previousSibling.className = `${IMG_CLASS_NAME} prev`;
   }
-  if (activedElem.nextSibling) {
+  if (activedElem !== activedElem.parentNode.lastChild) {
     activedElem.nextSibling.className = `${IMG_CLASS_NAME} next`;
   }
-  togglePrevBtn();
+  toggleBtn();
 };
 
-const initializeClasses = () => {
-  imgs = carouselContainer.querySelectorAll(".carousel__photo");
-  imgs[0].classList.add("active");
-  imgs[1].classList.add("next");
+const handleClick = (event) => {
+  [targetBtn, targetBlock] = event.path;
+  if (targetBtn.classList.contains("carousel__prev-i")) {
+    prevBtn = targetBtn;
+    movePrev();
+  }
+  if (targetBtn.classList.contains("carousel__next-i")) {
+    nextBtn = targetBtn;
+    moveNext();
+  }
 };
-
-function init() {
-  initializeClasses();
-  prevBtnList.forEach((btn) => btn.addEventListener("click", movePrev));
-  nextBtnList.forEach((btn) => btn.addEventListener("click", moveNext));
+const photoBlockCaseInit = () => {
+  photoBlocks.forEach((block) => {
+    imgs = block.querySelectorAll(".carousel__photo");
+    if (imgs.length > 1) {
+      imgs[0].classList.add("active");
+      imgs[1].classList.add("next");
+    }
+    const icons = block.querySelectorAll("i");
+    icons.forEach((item) => item.addEventListener("click", handleClick));
+  });
+};
+const photoDetailCaseInit = () => {
+  imgs = photoDetailCarousel.querySelectorAll(".carousel__photo");
+  if (imgs.length > 1) {
+    imgs[0].classList.add("active");
+    imgs[1].classList.add("next");
+  }
+  const icons = photoDetailCarousel.querySelectorAll("i");
+  icons.forEach((item) => item.addEventListener("click", handleClick));
+};
+if (photoBlocks) {
+  photoBlockCaseInit();
 }
-if (nextBtnList) {
-  init();
+if (photoDetailCarousel) {
+  photoDetailCaseInit();
 }
