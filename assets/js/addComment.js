9c26eb6f473653005dcff0214f2278151106b45b
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-04-06 13:17:06 +0900

    fixed error : add, delete comment

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -23,6 +23,8 @@ const COMMENT_LIST_CLASS = ".comment-list__container";
 const PHOTO_BLOCK_CLASS = ".comment-list__fake-container";
 
 const drawFakeElem = (comment, targetClass, target) => {
+  // length 3이상이면 커맨트리스트에만 추가
+  // 2까지는 두군대 다 보내야함
   const fakeCommentBlock = document
     .querySelector("#jsFakeBlock .comment-block")
     .cloneNode(true);
@@ -30,12 +32,13 @@ const drawFakeElem = (comment, targetClass, target) => {
   const editBtn = fakeCommentBlock.querySelector("#jsEditComment");
   const deleteBtn = fakeCommentBlock.querySelector("#jsDeleteComment");
   const currentComment = fakeCommentBlock.querySelector("#jsCurrentComment");
-  const editCommentForm = fakeCommentBlock.querySelector(
+  const editCommentInput = fakeCommentBlock.querySelector(
     "#jsEditCommentForm input"
   );
   const postEditUrl = `/api/${commentId}/edit-comment`;
   const postDelUrl = `/api/${commentId}/delete-comment`;
   if (target) {
+    console.log(comment, postEditUrl, postDelUrl);
     target.querySelector(targetClass).appendChild(fakeCommentBlock);
   } else {
     document.querySelector(targetClass).appendChild(fakeCommentBlock);
@@ -44,7 +47,7 @@ const drawFakeElem = (comment, targetClass, target) => {
   fakeCommentBlock.classList.remove("hide-element");
   fakeCommentBlock.classList.add("comment-block");
   currentComment.innerText = comment;
-  editCommentForm.value = comment;
+  editCommentInput.value = comment;
   link.setAttribute("href", "/me");
   editBtn.setAttribute("data-url", postEditUrl);
   deleteBtn.setAttribute("data-url", postDelUrl);
@@ -58,8 +61,9 @@ const addComment = (comment) => {
   if (numberStr) {
     [number] = numberStr;
   }
-  if (!number || (number < "3" && number > "0")) {
+  if (!number || (number > "0" && number < "3")) {
     const currentTargetBlock = commentNumberElem.parentNode.parentNode;
+    console.log(currentTargetBlock);
     drawFakeElem(comment, PHOTO_BLOCK_CLASS, currentTargetBlock);
   }
   drawFakeElem(comment, COMMENT_LIST_CLASS);
--- assets/js/deleteComment.js
+++ assets/js/deleteComment.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 let selectedBtn;
-let deletedList;
-let parent;
+let targetListBlock;
+let targetUl;
 let photoId;
 let commentNumberElem;
-let descriptionContainer;
-let index;
+let targetPhotoBlock;
 
 const decreaseNumber = () => {
   const number = commentNumberElem.innerText.split(" ")[1];
@@ -19,30 +18,24 @@ const decreaseNumber = () => {
     }
   }
 };
-
-const hideElement = () => {
-  const remained = parent.querySelectorAll(".comment-block");
-  console.log(remained);
-  if (remained.length === 2) {
-    if (remained.nextSibling) {
-      //남은 엘리먼트 1.페이크 2.남은 엘림 3.?
-      //지워진 다음이라서 언디파인-_-
-      index = 1;
-      //뒤에 엘림이 있다면 [1]
-      //없으면 마지막이니까 [2]
-    } else {
-      index = 0;
-    }
-    console.log(descriptionContainer.querySelectorAll(".comment-block")[index]);
-  } else if (remained.length === 1) {
-    console.log(descriptionContainer.querySelector(".comment-block"));
+//comment-list 에서 찍히면 photoBlock에서 지운다
+// 인덱스 2or 3 (페이크 블럭 떄문에)
+// 지워지기 전에 인덱스를 체크하고 지워야함
+const hideElement = (id) => {
+  const commentId = id;
+  const targetParent = targetPhotoBlock.querySelector(
+    ".comment-list__fake-container"
+  );
+  const target = targetParent.querySelector(
+    `[data-url="/api/${commentId}/edit-comment"]`
+  );
+  const remained = targetUl.querySelectorAll(".comment-block");
+  if (target && remained.length <= 4) {
+    const block = target.parentNode.parentNode.parentNode;
+    block.parentNode.removeChild(block);
   }
 
-  console.log(descriptionContainer.querySelectorAll(".comment-block"));
-  //length가 1이라면 description에서 블럭 찾아서 지우기
-  //2면 쿼리셀렉 올, 인덱스로 지우기
-
-  parent.removeChild(deletedList);
+  targetUl.removeChild(targetListBlock);
   decreaseNumber();
 };
 const deleteComment = async (targetCommentUrl) => {
@@ -58,7 +51,7 @@ const deleteComment = async (targetCommentUrl) => {
       },
     });
     if (response.status === 200) {
-      hideElement();
+      hideElement(commentId);
     }
   } catch (error) {
     console.log(error);
@@ -66,15 +59,13 @@ const deleteComment = async (targetCommentUrl) => {
 };
 const deleteCommentBtnHandler = (event) => {
   event.preventDefault();
-  [, , , , deletedList, parent] = event.path;
-  selectedBtn = event.currentTarget;
-
+  [, selectedBtn, , , targetListBlock, targetUl] = event.path;
   const targetCommentUrl = selectedBtn.getAttribute("data-url");
   deleteComment(targetCommentUrl);
 };
 function deleteCommentInit(id, elem) {
   commentNumberElem = elem;
-  descriptionContainer = commentNumberElem.parentNode.parentNode;
+  targetPhotoBlock = commentNumberElem.parentNode.parentNode;
   photoId = id;
   const deleteBtns = document.querySelectorAll("#jsDeleteComment");
   deleteBtns.forEach((btn) =>
