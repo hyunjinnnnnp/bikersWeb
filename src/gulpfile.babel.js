7b592b55ef3ebfcfafa99752140274b3ff445634
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-04-15 20:47:36 +0900

    gulp downgrade, primordials error: npm-shrinkwrap.json

--- src/gulpfile.babel.js
+++ src/gulpfile.babel.js
@@ -22,10 +22,23 @@ const paths = {
 };
 
 // eslint-disable-next-line import/prefer-default-export
-const clean = () => del(["static"]);
+//const clean = () => del(["static"]);
+gulp.task("clean", () => del(["static"]));
 
-const styles = () =>
-  gulp
+// const styles = () =>
+//   gulp
+//     .src(paths.styles.src)
+//     .pipe(sass().on("error", sass.logError))
+//     .pipe(
+//       autoprefixer({
+//         cascade: false,
+//       })
+//     )
+//     .pipe(minifyCSS())
+//     .pipe(gulp.dest(paths.styles.dest));
+
+gulp.task("styles", () => {
+  return gulp
     .src(paths.styles.src)
     .pipe(sass().on("error", sass.logError))
     .pipe(
@@ -35,20 +48,33 @@ const styles = () =>
     )
     .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
+});
+// const js = () =>
+//   gulp
+//     .src(paths.js.src)
+//     .pipe(bro({ transform: ["babelify"] }))
+//     .pipe(gulp.dest(paths.js.dest));
 
-const js = () =>
-  gulp
+gulp.task("js", () => {
+  return gulp
     .src(paths.js.src)
     .pipe(bro({ transform: ["babelify"] }))
     .pipe(gulp.dest(paths.js.dest));
+});
 
-const watchFiles = () => {
-  gulp.watch(paths.styles.watch, styles);
-  gulp.watch(paths.js.watch, js);
-};
+// const watchFiles = () => {
+//   gulp.watch(paths.styles.watch, styles);
+//   gulp.watch(paths.js.watch, js);
+// };
+gulp.task("watchFiles", () => {
+  gulp.watch(paths.styles.watch, ["js"]);
+  gulp.watch(paths.styles.watch, ["styles"]);
+});
 
-exports.production = gulp.series(clean, styles, js);
-exports.development = gulp.series(clean, styles, js, watchFiles);
+// exports.production = gulp.series(clean, styles, js);
+// exports.development = gulp.series(clean, styles, js, watchFiles);
+gulp.task("production", ["clean", "js", "styles"]);
+gulp.task("development", ["clean", "js", "styles", "watchFiles"]);
 //const dev = gulp.series(clean, styles, js);
 
 // export default dev;
