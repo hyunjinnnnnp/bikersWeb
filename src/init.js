ed526aa762c1c81767329bbc4b85be6f4286dd28
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-04-17 14:01:12 +0900

    fixed authentication callback url

--- src/init.js
+++ src/init.js
@@ -1,7 +1,4 @@
-import fs from "fs";
-import https from "https";
 import dotenv from "dotenv";
-import path from "path";
 import app from "./app";
 import "./db";
 import "./models/Photo";
@@ -15,10 +12,4 @@ const PORT = process.env.PORT || 4000;
 const handleListening = () =>
   console.log(`✅listening on: https://localhost:${PORT}`);
 
-// const httpsOptions = {
-//   key: fs.readFileSync(path.resolve("src", "mkcert", "key.pem")),
-//   cert: fs.readFileSync(path.resolve("src", "mkcert", "cert.pem")),
-// };
-
-// https.createServer(httpsOptions, app).listen(PORT, handleListening);
 app.listen(PORT, handleListening);
